c1d100d5fe1e62b5b9da877e5bc2d30b27d44526
NIKHIL-MAHAVEER <75113209+08NikhilPatil@users.noreply.github.com>
2021-11-26 16:35:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+class Student:
+    count = 0
+    def __init__(self, name, roll, maths, physics, chemistry):
+        Student.count += 1
+        self.roll = roll
+        self.name = name
+        self.maths = maths
+        self.physics = physics
+        self.chemistry = chemistry
+
+class Group:
+    def __init__(self):
+        self.members = [ ]
+
+    def add(self, student):
+        self.members.append(student)
+
+    def print_members(self):
+        for student in self.members:
+            print(student.name)
+
+student = Student('Anish', 4, 90, 95, 100)
+
+Student.count = 0
+f = open('scores.csv', 'r')
+f.readline()    # ignore the header
+students = [ ]
+for line in f:
+    roll, name, maths, physics, chemistry = line.strip().split(',')
+    roll, maths, physics, chemistry = int(roll), int(maths), int(physics), int(chemistry)
+    students.append(Student(name, roll, maths, physics, chemistry))
+f.close()
+print(Student.count)
